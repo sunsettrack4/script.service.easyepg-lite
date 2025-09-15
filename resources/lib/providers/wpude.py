5533621e3d0ea24d94f0418d8842e43aff7fb27a
Jan-Luca Neumann <sunsettrack4@gmail.com>
2025-09-15 11:56:00 +0200

    waipu.tv: Fix TypeError caused by strptime module

--- resources/lib/providers/wpude.py
+++ resources/lib/providers/wpude.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-import json, requests, time, uuid
+import json, requests, time
 
 
 def channels(data, session, headers={}):
@@ -39,7 +39,7 @@ def epg_main_converter(data, channels, settings, ch_id=None, genres={}):
     airings = []
 
     def get_time(string_item):
-        return str(datetime.strptime(string_item, "%Y-%m-%dT%H:%M:%SZ").timestamp()).split(".")[0]
+        return str(datetime(*(time.strptime(string_item, "%Y-%m-%dT%H:%M:%SZ")[0:6])).timestamp()).split(".")[0]
     
     for programme in item:
         
