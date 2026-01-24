f5360f12b47abc09a4285243074c350dce678e6f
DEvmIb <DEvmIb@users.noreply.github.com>
2026-01-24 23:37:18 +0100

    tvspf: Fix invalid episode_num
    the last patch wasn't a hyphen but an en dash. i left the hyphen in and added en and em dash support as well.

--- resources/lib/providers/tvspf.py
+++ resources/lib/providers/tvspf.py
@@ -206,7 +206,7 @@ def epg_advanced_converter(item, data, cache, settings):
         if len(se_info) == 1 and "Folge " in se_info[0]:
             e_num, p_num = correct_num(se_info[0].replace("Folge ", ""))
         elif len(se_info) > 1 and "Folge " in se_info[1]:
-            e_num_fix = se_info[1].replace("Folge ", "").split("/")[0].split("+")[0].split(";")[0].split(",")[0].split("-")[0]
+            e_num_fix = se_info[1].replace("Folge ", "").split("/")[0].split("+")[0].split(";")[0].split(",")[0].split("-")[0].split(chr(8211))[0].split(chr(8212))[0]
             if "." in e_num_fix: e_num_fix = e_num_fix.split(".")[1]
             e_num, p_num = correct_num(e_num_fix)
 
